b55c51856c78a29be4a3fb9369240dbac612372b
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-29 12:35:49 -0400

    Add logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-
+import Container from '../containers/Container';
+import Logo from '../../assets/img/logo.png';
 const header = () => (
   <div className='Header'>
+    <Container>
+      <div className="Logo">
+        <img src={Logo} alt="Logo"/>
+      </div>
+    </Container>
   </div>
 );
 
---
+++ src/components/containers/Container.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const layout = ( props ) => (
+    <div className='container'>{props.children}</div>
+);
+
+export default layout;
