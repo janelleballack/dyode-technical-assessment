1b68f4201b54562165616330fda6826b3a3f9e17
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-29 09:19:19 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Topbar
+      Header
+      Content
+        Hero Slider
+        Three Up Grid
+        Four Up Slider
+        Hero Image
+        Four Up Grid
+        Subscribe Banner
+      Footer
+        
+    </div>
+  );
+}
+
+export default App;
