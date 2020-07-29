1922bd073a694b66d7923625cafc883c7d3ccd67
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-29 10:02:42 -0400

    Add Topbar component

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+const Topbar = (props) => (
+  <div className='Topbar'>
+   <p className='Text'>FREE SHIPPING ON ALL ORDERS OVER $75</p>
+  </div>
+);
+ 
+export default Topbar;
