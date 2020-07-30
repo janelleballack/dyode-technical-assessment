ae36d9ae2c81adbd7c6357844ca5da9300318c01
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-30 17:25:15 -0400

    Add Newsletter Component

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
+import IconEmail from '../../../src/assets/img/email-icon.png';
+import Container from '../../containers/Container';
+
 const Newsletter = () => (
   <div className="Newsletter">
-    <img className="Newsletter__image" src="" alt="Images" />
-    <p className="Newsletter__title">Sign up.& stay connected</p>
-    <p className="Newsletter__description">Sign up for the newsletter and get 20% off! Get access to exclusive offers and be the first to know when new stuff drops.</p>
-    <form className="Newsletter__form">
-      <input className="Newsletter__input" type="text" name="name" placeholder="Your Email Address"/>
-      <input className="Newsletter__submitBtn" type="submit" value="Subscribe" />
-    </form>
+    <Container>
+      <p className="Newsletter__title">
+      <img className="Newsletter__icon" src={IconEmail} alt="Images" />
+        <span>Sign up & Stay Connected</span>
+      </p>
+      <form className="Newsletter__form">
+        <p className="Newsletter__description">Sign up for the newsletter and get 20% off! Gain access to exclusive offers and be the first to know when new stuff drops!</p>
+        <input className="Newsletter__input" type="text" name="name" placeholder="Your Email Address"/>
+        <input className="Newsletter__submitBtn" type="submit" value="Subscribe" />
+      </form>
+    </Container>
   </div> 
 );
 
