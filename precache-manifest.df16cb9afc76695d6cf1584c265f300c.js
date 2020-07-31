abeb4cffa51235829c1b829a02d5d50ee6ef2458
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-31 09:01:53 -0400

    Updates

---
+++ precache-manifest.df16cb9afc76695d6cf1584c265f300c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38dabd96f7369b08ead7f5997aef6744",
+    "url": "/dyode-technical-assessment/index.html"
+  },
+  {
+    "revision": "687271c47c76d99e1164",
+    "url": "/dyode-technical-assessment/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "ec6c06080a6f02ca3bea",
+    "url": "/dyode-technical-assessment/static/css/main.84ebe688.chunk.css"
+  },
+  {
+    "revision": "687271c47c76d99e1164",
+    "url": "/dyode-technical-assessment/static/js/2.2ca6d45a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dyode-technical-assessment/static/js/2.2ca6d45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6c06080a6f02ca3bea",
+    "url": "/dyode-technical-assessment/static/js/main.937929c4.chunk.js"
+  },
+  {
+    "revision": "50c546c140efc7448682",
+    "url": "/dyode-technical-assessment/static/js/runtime-main.e84e8236.js"
+  },
+  {
+    "revision": "b281bf6ba680e2636da446a56dbbb193",
+    "url": "/dyode-technical-assessment/static/media/accessories-image-mobile.b281bf6b.jpg"
+  },
+  {
+    "revision": "3181decb286654080050e64a20ee4979",
+    "url": "/dyode-technical-assessment/static/media/accessories-image.3181decb.jpg"
+  },
+  {
+    "revision": "79e861a65b21a00844ba365b177b4e24",
+    "url": "/dyode-technical-assessment/static/media/hero-image-1-mobile.79e861a6.jpg"
+  },
+  {
+    "revision": "b42a7ccd945f933be15eecdf1287d77d",
+    "url": "/dyode-technical-assessment/static/media/hero-image-1.b42a7ccd.jpg"
+  },
+  {
+    "revision": "cfaec045aa46f59d742faf9355cc9c42",
+    "url": "/dyode-technical-assessment/static/media/hero-image-2-mobile.cfaec045.jpg"
+  },
+  {
+    "revision": "3adcc50f25c8ecb22d91cef8d5bd6aed",
+    "url": "/dyode-technical-assessment/static/media/hero-image-2.3adcc50f.jpg"
+  },
+  {
+    "revision": "21572e5076f2d4821b58740fa90d58b1",
+    "url": "/dyode-technical-assessment/static/media/mens-image.21572e50.jpg"
+  },
+  {
+    "revision": "73b1f38046430359a7f7944ff6c23c51",
+    "url": "/dyode-technical-assessment/static/media/product-image-1.73b1f380.jpg"
+  },
+  {
+    "revision": "2637bbe60385449e43773f59f6ba6eec",
+    "url": "/dyode-technical-assessment/static/media/social-1.2637bbe6.jpg"
+  },
+  {
+    "revision": "a79eb98879467339b9c708c31b0f416e",
+    "url": "/dyode-technical-assessment/static/media/social-2.a79eb988.jpg"
+  },
+  {
+    "revision": "4f51799f30d4f85ebf85f9eab37d5372",
+    "url": "/dyode-technical-assessment/static/media/social-3.4f51799f.jpg"
+  },
+  {
+    "revision": "7e7f4aa4af8979a1171500a952c437b9",
+    "url": "/dyode-technical-assessment/static/media/social-4.7e7f4aa4.jpg"
+  },
+  {
+    "revision": "d017f63f42a13ef2b01715f019c3f971",
+    "url": "/dyode-technical-assessment/static/media/social-5.d017f63f.jpg"
+  },
+  {
+    "revision": "c10f625511647413250d7b056e6f0d37",
+    "url": "/dyode-technical-assessment/static/media/womens-image.c10f6255.jpg"
+  }
+]);
