12637ef53ea337e21b81096910e4d61051e42924
Janelle Ballack <janelleballack@MacBook-Pro.fios-router.home>
2020-07-31 14:13:51 -0400

    Add slick slider and update styles for product images

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
+const ProductCard = () => {
+const product =  
+    {title       : 'Product Title', 
+     description : 'Women’s T-Shirt',
+     price       : '$19.99',
+     url  :`${require("../../assets/img/product-image-1.jpg")}`}
+    
 
 
-const ProductCard = (props) => (
+return(
+
   <div className="ProductCard">
-    <img className="ProductCard__image" src={props.url} alt="Images" />
+    <img className="ProductCard__image" src={product.url} alt="Images" />
       <div className="ProductCard__info">
-      <p className="ProductCard__title">{props.title}</p>
-      <p className="ProductCard__description">{props.description}</p>
-      <p className="ProductCard__price">{props.price}</p>
+      <p className="ProductCard__title">{product.title}</p>
+      <p className="ProductCard__description">{product.description}</p>
+      <p className="ProductCard__price">{product.price}</p>
     </div> 
   </div>
-);
-
+)
+};
 export default ProductCard;
  
--- src/components/ProductCarousel/ProductCarousel.js
+++ src/components/ProductCarousel/ProductCarousel.js
@@ -1,28 +1,80 @@
 import React from "react";
 
-import { Carousel }  from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import Aux from '../../hoc/Aux';
-import ProductGrid from "../ProductGrid/ProductGrid";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Container from '../../containers/Container';
+import ProductCard from "../ProductCard/ProductCard";
+import LeftArrow from "../../assets/img/arrow-left.png";
+import RightArrow from "../../assets/img/arrow-right.png";
 
 const ProductCarousel= () => {
-  var times = 3; // or more
+  var times = 8; // or more
   var slides = Array(times);
   for(var i = 0; i < times; i++){
-    slides[i] = <Aux key={i}><ProductGrid /></Aux>
+    slides[i] = <div key={i}><ProductCard /></div>
 }
  
+ const SampleNextArrow = (props) => {
+  const { className, onClick } = props;
   return (
-    <Aux>
-    <p className="ProductCarousel__title">New Arrivals</p>
-      <Carousel
-        showThumbs={false} 
-        showStatus={false} 
-        showIndicators={false} 
-        className="ProductCarousel">
-        {slides}
-      </Carousel>
-    </Aux>
+    <div
+      className={className}
+      onClick={onClick}> <img src={RightArrow} alt="" />
+    </div>
+  );
+}
+
+const SamplePrevArrow = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}><img src={LeftArrow} alt="" />
+    </div>
+  );
+}
+
+var settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      className: "Slider",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        }
+      ]
+    };
+
+  return (
+    <div className="ProductCarousel">
+      <Container>
+      <p className="ProductCarousel__title">New Arrivals</p>
+        <Slider {...settings}>
+          {slides}
+        </Slider>
+      </Container>
+    </div>
   );
 }
  
